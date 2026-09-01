fc4171d6d93eb086fe912dc4ad567ac8572e92c8
vinothini149 <vinothiniav1409@gmail.com>
2026-09-01 11:23:22 +0530

    Program 4

--- student.sh
+++ student.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 touch filename.txt file1.txt file2.txt file3.txt
+name: Test
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  test:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v4
+      - run: echo "Hello"
